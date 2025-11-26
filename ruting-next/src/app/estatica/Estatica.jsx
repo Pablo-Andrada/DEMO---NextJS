813e60b92b85b4e190ab489c017413defd0c1ed3
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-11-26 02:07:22 -0300

    Rutas estáticas en NextJS Routing

---
+++ ruting-next/src/app/estatica/Estatica.jsx
@@ -0,0 +1,9 @@
+export const Estatica = () => {
+    return (
+        <div>
+            Estamos en nuestra ruta estática: http://localhost:3000/estatica
+        </div>
+    )
+}
+
+export default Estatica;
