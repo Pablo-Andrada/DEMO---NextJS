976fd130c2b6518657ab25eb89699133e4911d1e
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-12-12 23:20:52 -0300

    working

--- ruting-next/src/app/profile/SlugProfile.jsx
+++ ruting-next/src/app/profile/SlugProfile.jsx
@@ -1,7 +1,7 @@
-export const Profile = ()=>{
+export const Profile = ({params})=>{
     return(
         <div>
-           
+            <h1>Soy el slug:{params.slug}</h1>
         </div>
     );
 }
